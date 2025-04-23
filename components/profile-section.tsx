65d06596e05b2ad3c7a1aa04d81a2ee0d9babb61
melicg93 <melisssa.cantero@gmail.com>
2025-04-23 13:04:27 +0100

    Update profile-section.tsx

--- components/profile-section.tsx
+++ components/profile-section.tsx
@@ -8,10 +8,10 @@ export default function ProfileSection() {
       </CardHeader>
       <CardContent className="pt-6 dark:text-slate-200">
         <p className="text-gray-700 dark:text-slate-300">
-          Dynamic, hardworking and honest individual who is always willing to learn new skills. Friendly, helpful,
-          polite and responsible person with a good sense of humor who is able to work independently in a busy
-          fast-paced environment and also within a team setting.
-        </p>
+          Dynamic and result-driven hospitality professional with over 10 years of experience in front office operations,
+customer service, and team supervision. Proven ability to adapt to diverse environments, lead high-performing teams, and
+deliver exceptional guest experience. Fluent in Spanish, English and Portuguese, with international work experience in 
+Ireland and Paraguay.</p>
       </CardContent>
     </Card>
   )
